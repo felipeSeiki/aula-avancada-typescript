494f0651884233a436dcb39c5880380105afb3d0
felipeseiki <felipe.hashiguti@gmail.com>
2025-02-27 23:20:16 -0300

    Criando um filtro de veiculo por ano, marca e modelo

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-import { EstoqueVeiculos } from "./classes/EstoqueVeiculos";
+import { filtrarPorAno, filtrarPorMarca, filtrarPorModelo } from "./classes/FiltroVeiculo";
+import { Veiculo } from "./interface/Veiculo";
 
-const estoque = new EstoqueVeiculos();
+const veiculos: Veiculo[] =[
+    {modelo: "Civic", ano: 2020, marca: "Honda"},
+    {modelo: "Corolla", ano: 2021, marca: "Toyota"},
+    {modelo: "CB 500", ano: 2022, marca: "Honda"},
+];
 
-estoque.adicionarEstoque("Civic", 5);
-estoque.adicionarEstoque("CB 500", 2);
+console.log("Veículos de 2021: ", filtrarPorAno(veiculos, 2021));
+console.log("Veículos da marca Honda: ", filtrarPorMarca(veiculos, "Honda"));
+console.log("Veículos modelo Civic: ", filtrarPorModelo(veiculos, "Civic"));
 
-console.log("Estoque após adição: ", estoque.consultaEstoque("Civic"));
-
-estoque.removeEstoque("Civic", 3);
-
-console.log("Estoque após remoção: ", estoque.consultaEstoque("Civic"));
 
 
